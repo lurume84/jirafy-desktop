03035c221b6784b577880178f0262e2825aac4ed
lurume84 <lurume84@gmail.com>
2019-11-28 16:06:06 +0100

    make project config relative to projectdir so cmake build works

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 rmdir /s /q build
 mkdir build
 cd build
-cmake -G "Visual Studio 15 2017" -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
 pause
