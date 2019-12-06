d607474de9aa60fc87899a0ec38fbecf9fcf9fed
Luis Ruiz <roco65@gmail.com>
2019-12-07 00:35:09 +0100

    set versioning during build process

--- version.ps1
+++ version.ps1
@@ -2,6 +2,5 @@ $x='0'
 $y='1'
 $z='6'
 
-((Get-Content -path .\src\DesktopApp\resources\win\resources.rc -Raw) -replace '0.0.0.0', ($x + '.' + $y + '.' + $z + '.0')) | Set-Content -Path .\src\DesktopApp\resources\win\resources.rc
-((Get-Content -path .\src\DesktopApp\resources\win\resources.rc -Raw) -replace '0,0,0,0', ($x + ',' + $y + ',' + $z + ',0')) | Set-Content -Path .\src\DesktopApp\resources\win\resources.rc
+((Get-Content -path .\src\DesktopApp\resources\win\resources.rc -Raw) -replace '0,0,0,0', ($x + ',' + $y + ',' + $z + ',0') -replace '0.0.0.0', ($x + '.' + $y + '.' + $z + '.0')) | Set-Content -Path .\src\DesktopApp\resources\win\resources.rc
 ((Get-Content -path .\src\DesktopInstaller\Installer.nsi -Raw) -replace '0.0.0.0', ($x + '.' + $y + '.' + $z + '.0')) | Set-Content -Path .\src\DesktopInstaller\Installer.nsi
